e6711d73ee05d51a0046fa4ff810a54013a49c77
Francis LE ROY <fle-roy@student.42.fr>
2018-02-05 13:21:51 +0100

    Can wrap line and move with arrow key on terminal that doesn't support wrapping

--- src/main.c
+++ src/main.c
@@ -6,11 +6,12 @@
 /*   By: fle-roy <fle-roy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/02 10:40:09 by fle-roy           #+#    #+#             */
-/*   Updated: 2018/02/03 16:40:17 by fle-roy          ###   ########.fr       */
+/*   Updated: 2018/02/05 12:24:25 by fle-roy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_sh.h"
+#include <fcntl.h>
 
 t_ft_sh	*get_ft_shell(void)
 {
@@ -19,19 +20,33 @@ t_ft_sh	*get_ft_shell(void)
 	return (&shell);
 }
 
+void	init_debug(t_ft_sh *shell, const char *path)
+{
+	if ((shell->debug_tty = open(path, O_WRONLY)) < 0)
+		return ;
+	ft_fprintf(shell->debug_tty, "-------------------------------------\n");
+}
+
 int		main(int argc, const char **argv, const char **environ)
 {
-	dbuf_init(&get_ft_shell()->buf);
-	(void)argc;
-	(void)argv;
+	t_ft_sh *shell;
+
 	(void)environ;
+	shell = get_ft_shell();
+	shell->debug_tty = -1;
+	dbuf_init(&shell->buf);
+	if (argc == 3 && !ft_strcmp("-d", argv[1]))
+		init_debug(shell, argv[2]);
 	if (!is_env_correct())
 		return (1);
 	apply_terminal_setting(0);
+	get_screen_size();
 	display_prompt(0);
 	char *res = read_command();
 	ft_printf("\nTyped : %s\n", res);
 	free(res);
 	apply_terminal_setting(1);
+	if (shell->debug_tty > 0)
+		close(shell->debug_tty);
 	return (0);
 }
--- src/special_touch.c
+++ src/special_touch.c
@@ -6,7 +6,7 @@
 /*   By: fle-roy <fle-roy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/03 15:28:41 by fle-roy           #+#    #+#             */
-/*   Updated: 2018/02/03 18:53:42 by fle-roy          ###   ########.fr       */
+/*   Updated: 2018/02/05 13:20:53 by fle-roy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,29 @@ void		spt_backspace(unsigned long touch)
 
 void		spt_arrow(unsigned long touch)
 {
-	t_ft_sh *shell;
+	t_ft_sh *sh;
 
-	shell = get_ft_shell();
-	if (touch == T_LARR && shell->cursor > 0)
+	sh = get_ft_shell();
+	if (touch == T_LARR && sh->cursor > 0)
 	{
-		exec_term_command(TC_MOVELEFT);
-		shell->cursor--;
+		if (!((sh->prompt_size + sh->cursor) % (sh->x_size)))
+		{
+			exec_term_command(TC_MOVEUP);
+			exec_term_command_p(TC_MOVENRIGHT, 0, sh->x_size);
+		}
+		else
+			exec_term_command(TC_MOVELEFT);
+		sh->cursor--;
 	}
-	else if (touch == T_RARR && shell->cursor < shell->buf.cursor)
+	else if (touch == T_RARR && sh->cursor < sh->buf.cursor)
 	{
-		exec_term_command(TC_MOVERIGHT);
-		shell->cursor++;
+		if (!((sh->prompt_size + sh->cursor) % (sh->x_size - 1)))
+		{
+			exec_term_command_p(TC_MOVENLEFT, 0, sh->x_size);
+			exec_term_command(TC_MOVEDOWN);
+		}
+		else
+			exec_term_command(TC_MOVERIGHT);
+		sh->cursor++;
 	}
 }
