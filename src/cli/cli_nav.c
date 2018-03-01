015df31b2bd7aed68f64de122f9290a2aa3cf251
Francis LE ROY <francis.leroy.ch@gmail.com>
2018-03-01 16:38:05 +0100

    Bug with navigation and multiligne command fixed

--- src/cli/cli_nav.c
+++ src/cli/cli_nav.c
@@ -6,11 +6,19 @@
 /*   By: fle-roy <fle-roy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/05 17:55:43 by fle-roy           #+#    #+#             */
-/*   Updated: 2018/02/25 18:10:18 by fle-roy          ###   ########.fr       */
+/*   Updated: 2018/03/01 16:37:23 by fle-roy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_sh.h"
+
+int			is_a_new_line(void)
+{
+	t_ft_sh *sh;
+
+	sh = get_ft_shell();
+	return (sh->buf.buf[sh->cursor - 1] == '\n');
+}
 
 static void	skip_in_terminal(unsigned long touch)
 {
@@ -19,7 +27,8 @@ static void	skip_in_terminal(unsigned long touch)
 	int tmp;
 
 	dir = ((touch == T_SLARR ? -1 : 1));
-	if (touch == T_SBARR || touch == T_STARR)
+	if (touch == T_SBARR || touch == T_STARR ||
+		(touch == T_SLARR && is_a_new_line()))
 		return ;
 	sh = get_ft_shell();
 	while (sh->cursor + (dir > 0) > 0
@@ -38,7 +47,7 @@ static void	home_or_end_touch(unsigned long touch)
 	t_ft_sh *sh;
 
 	sh = get_ft_shell();
-	while ((touch == T_HOME && sh->cursor) ||
+	while ((touch == T_HOME && sh->cursor && !is_a_new_line()) ||
 		(touch == T_END && sh->cursor < sh->buf.cursor))
 		move_in_terminal((touch == T_HOME ? T_LARR : T_RARR), 1);
 }
@@ -58,7 +67,7 @@ void		move_in_terminal(unsigned long touch, int should_update_buf)
 	t_ft_sh *sh;
 
 	sh = get_ft_shell();
-	if (touch == T_LARR && sh->cursor > 0)
+	if (touch == T_LARR && sh->cursor > 0 && !is_a_new_line())
 	{
 		if (sh->is_a_tty && ((sh->prompt_size + sh->cursor) % (sh->x_size)) == 0)
 		{
