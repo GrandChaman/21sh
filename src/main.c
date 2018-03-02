9fd27a3ba622c99b8b0d3153f33dd6d7bffcfd4d
VictorBaudot <vbaudot@student.42.fr>
2018-03-02 11:18:25 +0100

    No segfault

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fle-roy <fle-roy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/02 10:40:09 by fle-roy           #+#    #+#             */
-/*   Updated: 2018/03/02 10:40:23 by vbaudot          ###   ########.fr       */
+/*   Updated: 2018/03/02 11:17:02 by vbaudot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,33 +31,27 @@ void main_routine(t_list **head, int status)
 	char *cmd;
 	t_ft_sh *shell;
 	t_parser *parser;
-	char	**args;
 	int		x;
+	int 	nb;
 
 	shell = get_ft_shell();
 	//load_history(shell, 0);
 	while (status)
 	{
-		ft_printf("\\\\\\\\\\\\\\\\\\\\\\\\\\\n");
 		ft_fprintf(shell->debug_tty, "YAY\n");
 		ft_fprintf(shell->debug_tty, "%p\n", shell->history);
 		cmd = read_command(NULL, NULL);
-		args = ft_split_whitespaces(cmd);
 		parser = get_parser(cmd);
+		nb = parser[0].nb;
 		x = -1;
-		while (parser[++x].cmd[0])
+		while (++x < nb)
 			status = execute(parser[x], head);
-		//status = execute(args, head);
 		//add_to_history(shell, cmd);
 		//load_history(shell, 1);
 		ft_fprintf(shell->debug_tty, "YAY\n");
 		//ft_printf("%s%s\n", (!shell->is_a_tty ? "" : "\nTyped : "),cmd);
 		free_parser(parser);
 		free(cmd);
-		x = -1;
-		while (args[++x])
-			free(args[x]);
-		free(args);
 	}
 }
 
@@ -73,7 +67,7 @@ int		main(int argc, const char **argv, char **env)
 	if (!is_env_correct())
 		return (1);
 	head = create_list_from_env(env);
-	ft_lstprint(&head);
+	//ft_lstprint(&head);
 	cli_loader(0);
 	main_routine(&head, 1);
 	cli_loader(1);
