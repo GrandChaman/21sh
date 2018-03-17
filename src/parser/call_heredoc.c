80a84ba046f0de61116f902cecd0cc135d4e01a2
rfautier <rfautier@student.42.fr>
2018-03-17 18:59:18 +0100

    norme heredoc

--- src/parser/call_heredoc.c
+++ src/parser/call_heredoc.c
@@ -1,11 +1,12 @@
 #include "ft_sh.h"
 
-static int size_str(t_vari var, char *original)
+static int	size_str(t_vari var, char *original)
 {
 	int nb;
 
 	nb = 0;
-	while (original[var.i] && (original[var.i] != ' ' && original[var.i] != '\t'))
+	while (original[var.i] && (original[var.i] != ' ' &&
+		original[var.i] != '\t'))
 	{
 		var.i++;
 		nb++;
@@ -13,42 +14,26 @@ static int size_str(t_vari var, char *original)
 	return (nb);
 }
 
-
-void	call_heredoc(t_vari var, char *original)
+static char	*search_str(t_vari var, char *original)
 {
-	int size;
-	char *str;
-	int o;
-	char *tmp = NULL;
-	char *path_file;
-	int fd;
-	char *tmp2;
+	int		o;
+	char	*str;
 
 	o = 0;
-	size = size_str(var, original);
-	str = malloc(sizeof(char) * size + 1);
-	while (original[var.i] && (original[var.i] != ' ' && original[var.i] != '\t'))
+	str = malloc(sizeof(char) * size_str(var, original) + 1);
+	while (original[var.i] && (original[var.i] != ' ' &&
+		original[var.i] != '\t'))
 	{
 		str[o] = original[var.i];
 		var.i++;
 		o++;
 	}
 	str[o] = '\0';
-	tmp = read_command(NULL, 0, 1, 0);
-	tmp2 = ft_itoa(var.heredoc);
-	path_file = ft_strjoin("/tmp/21sh_heredoc", tmp2);
-	free(tmp2);
-	if ((fd = open(path_file,  O_RDWR | O_CREAT | O_EXCL | O_APPEND, 0777)) == -1)
-	{
-		if ((fd = open(path_file, O_WRONLY | O_TRUNC, 0777)) == -1)
-		{
-			exit(0);
-		}
-		close(fd);
-		if ((fd = open(path_file,  O_RDWR | O_APPEND, 0777)) == -1)
-			exit(0);
-	}
-	free(path_file);
+	return (str);
+}
+
+static void	final_heredoc(char *tmp, char *str, char *tmp2, int fd)
+{
 	while (ft_strcmp(str, tmp) != 0)
 	{
 		tmp2 = ft_strjoin(tmp, "\n");
@@ -57,6 +42,32 @@ void	call_heredoc(t_vari var, char *original)
 		free(tmp);
 		tmp = read_command(NULL, 0, 1, 0);
 	}
+}
+
+void		call_heredoc(t_vari var, char *original)
+{
+	char	*str;
+	char	*tmp;
+	char	*path_file;
+	int		fd;
+	char	*tmp2;
+
+	str = search_str(var, original);
+	tmp = read_command(NULL, 0, 1, 0);
+	tmp2 = ft_itoa(var.heredoc);
+	path_file = ft_strjoin("/tmp/21sh_heredoc", tmp2);
+	free(tmp2);
+	if ((fd = open(path_file, O_RDWR | O_CREAT | O_EXCL |
+		O_APPEND, 0777)) == -1)
+	{
+		if ((fd = open(path_file, O_WRONLY | O_TRUNC, 0777)) == -1)
+			exit(0);
+		close(fd);
+		if ((fd = open(path_file, O_RDWR | O_APPEND, 0777)) == -1)
+			exit(0);
+	}
+	free(path_file);
+	final_heredoc(tmp, str, tmp2, fd);
 	free(tmp);
 	free(str);
 	close(fd);
