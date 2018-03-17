dafa3972ff37cce3e4207c1703bcbbc5b1edc0a5
rfautier <rfautier@student.42.fr>
2018-03-17 16:26:59 +0100

    norme init_parser et get_parserr

--- src/parser/get_parser.c
+++ src/parser/get_parser.c
@@ -7,25 +7,18 @@ t_parser	*get_parser(char *original) //Pour victor
 
 	stock = is_correct_talk(original);
 	if (stock == 0)
-	{
-//		printf("\nFails\n");
 		return (NULL);
-	}
 	if (stock < 0)
-	{
-//		printf("Francis complet\n");
 		return (NULL);
-	}
 	if (stock == 1)
 	{
-	//	printf("\nOK\n");
 		stock = count_cmd(original);
-	//	printf("------------------------------------------- nb commandes = %d\n",stock);
+		printf("------------------------------------------- nb commandes = %d\n",stock);
 		parser = malloc(sizeof(t_parser) * stock);
 		init_parser(parser, stock);
 		split_evoluted(parser, original);
 		fill_parser(parser, original);
-	//	print_parser(parser, stock);
+		print_parser(parser, stock);
 		return (parser);
 	}
 	return (NULL);
--- src/parser/init_parser.c
+++ src/parser/init_parser.c
@@ -1,6 +1,6 @@
 #include "ft_sh.h"
 
-void	init_parser(t_parser *parser, int nb)
+void		init_parser(t_parser *parser, int nb)
 {
 	int i;
 
@@ -13,18 +13,69 @@ void	init_parser(t_parser *parser, int nb)
 		parser[i].close_stdout = 0;
 		parser[i].close_stderr = 0;
 		parser[i].input.pipe = 0;
-		parser[i].input.meta = NULL; // a initialiser plus tard
+		parser[i].input.meta = NULL;
 		parser[i].output.pipe = 0;
-		parser[i].output.meta = NULL; // a initialiser plus tard
+		parser[i].output.meta = NULL;
 		i++;
 	}
 }
 
-void	free_parser(t_parser *parser)
+static void	free_cmd(t_parser *parser, int i)
+{
+	int o;
+
+	o = 0;
+	if (parser[i].cmd)
+	{
+		while (parser[i].cmd[o])
+		{
+			free(parser[i].cmd[o]);
+			o++;
+		}
+		free(parser[i].cmd);
+	}
+}
+
+static void	free_input_meta(t_parser *parser, int i)
+{
+	int o;
+
+	o = 0;
+	if (parser[i].input.meta)
+	{
+		while (parser[i].input.meta)
+		{
+			free(parser[i].input.meta[o].name);
+			if (parser[i].input.meta[o].next_exist == 0)
+				break ;
+			o++;
+		}
+		free(parser[i].input.meta);
+	}
+}
+
+static void	free_output_meta(t_parser *parser, int i)
+{
+	int o;
+
+	o = 0;
+	if (parser[i].output.meta)
+	{
+		while (parser[i].output.meta)
+		{
+			free(parser[i].output.meta[o].name);
+			if (parser[i].output.meta[o].next_exist == 0)
+				break ;
+			o++;
+		}
+		free(parser[i].output.meta);
+	}	
+}
+
+void		free_parser(t_parser *parser)
 {
 	int i;
 	int nb;
-	int o;
 
 	i = 0;
 	nb = 0;
@@ -34,40 +85,9 @@ void	free_parser(t_parser *parser)
 			nb = parser[0].nb;
 		while (i < nb)
 		{
-			o = 0;
-			if (parser[i].cmd)
-			{
-				while (parser[i].cmd[o])
-				{
-					free(parser[i].cmd[o]);
-					o++;
-				}
-				free(parser[i].cmd);
-			}
-			if (parser[i].input.meta)
-			{
-				o = 0;
-				while (parser[i].input.meta)
-				{
-					free(parser[i].input.meta[o].name);
-					if (parser[i].input.meta[o].next_exist == 0)
-						break ;
-					o++;
-				}
-				free(parser[i].input.meta);
-			}
-			if (parser[i].output.meta)
-			{
-				o = 0;
-				while (parser[i].output.meta)
-				{
-					free(parser[i].output.meta[o].name);
-					if (parser[i].output.meta[o].next_exist == 0)
-						break ;
-					o++;
-				}
-				free(parser[i].output.meta);
-			}
+			free_cmd(parser, i);
+			free_input_meta(parser, i);
+			free_output_meta(parser, i);
 			i++;
 		}
 		free(parser);
