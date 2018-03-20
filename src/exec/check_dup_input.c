686bbc51ace7583c5d1931af5140f65ff06ab265
Romain FAUTIER <rfautier@e1r7p14.42.fr>
2018-03-20 17:47:29 +0100

    norme dup et pipe

--- src/exec/check_dup_input.c
+++ src/exec/check_dup_input.c
@@ -1,52 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_dup_input.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rfautier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/20 17:16:10 by rfautier          #+#    #+#             */
+/*   Updated: 2018/03/20 17:22:06 by rfautier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_sh.h"
 
-int	check_dup_input(t_parser *parser, int x)
+static void	is_fd(int x, int i, t_parser *parser)
 {
-	int i;
+	int stock;
+
+	stock = 0;
+	if (!(parser[x].input.meta[i].name[1] &&
+		parser[x].input.meta[i].name[1] == '-'))
+	{
+		ft_easy_input(&stock, x, i, parser);
+		if (parser[x].input.meta[i].name[1])
+			dup2(stock, parser[x].input.meta[i].name[1] - 48);
+		else
+			dup2(stock, 0);
+	}
+}
+
+static void	is_heredoc_2(int x, t_parser *parser, int i)
+{
+	char	*str;
+	char	*str2;
+	int		fd;
+
+	str = "/tmp/21sh_heredoc";
+	str2 = ft_itoa(parser[x].input.meta[i].heredoc_number);
+	str = ft_strjoin(str, str2);
+	if ((fd = open(str, O_RDWR, 0777)) == -1)
+		exit(0);
+	free(str);
+	free(str2);
+	dup2(fd, 0);
+}
+
+static int	is_normal(int x, t_parser *parser, int i)
+{
 	int fd;
 	int stock;
-	char *str;
-	char *str2;
+
+	stock = 0;
+	if ((fd = open(parser[x].input.meta[i].name, O_RDWR, 0777)) == -1)
+	{
+		ft_printf("21sh :no such file or directory\n");
+		return (0);
+	}
+	ft_easy_input(&stock, x, i, parser);
+	dup2(fd, stock);
+	return (1);
+}
+
+int			check_dup_input(t_parser *parser, int x)
+{
+	int i;
 
 	i = 0;
 	if (parser[x].input.meta)
 	{
 		while (parser[x].input.meta)
 		{
-			if (parser[x].input.meta[i].name && parser[x].input.meta[i].name[0] == '&')
-			{
-				if (!(parser[x].input.meta[i].name[1] && parser[x].input.meta[i].name[1] == '-'))
-				{
-					ft_easy_input(&stock, x, i, parser);
-					if (parser[x].input.meta[i].name[1])
-						dup2(stock, parser[x].input.meta[i].name[1] - 48);
-					else
-						dup2(stock, 0);
-				}
-			}
+			if (parser[x].input.meta[i].name &&
+				parser[x].input.meta[i].name[0] == '&')
+				is_fd(x, i, parser);
 			else
 			{
 				if (parser[x].input.meta[i].heredoc_number)
-				{
-					str = "/tmp/21sh_heredoc";
-					str2 = ft_itoa(parser[x].input.meta[i].heredoc_number);
-					str = ft_strjoin(str, str2);
-					if ((fd = open(str,  O_RDWR, 0777)) == -1)
-						exit(0);
-					free(str);
-					free(str2);
-					dup2(fd, 0);
-				}
-				else
-				{
-					if ((fd = open(parser[x].input.meta[i].name,  O_RDWR, 0777)) == -1)
-					{
-						ft_printf("21sh :no such file or directory\n");
-						return (0);
-					}
-					ft_easy_input(&stock, x, i, parser);
-					dup2(fd, stock);
-				}
+					is_heredoc_2(x, parser, i);
+				else if (!(is_normal(x, parser, i)))
+					return (0);
 			}
 			if (parser[x].input.meta[i].next_exist == 0)
 				break ;
@@ -56,7 +87,7 @@ int	check_dup_input(t_parser *parser, int x)
 	return (1);
 }
 
-void	ft_easy_input(int *stock, int x, int i, t_parser *parser)
+void		ft_easy_input(int *stock, int x, int i, t_parser *parser)
 {
 	if (parser[x].input.meta[i].stdin)
 		*stock = 0;
