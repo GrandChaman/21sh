764b960dcef4f5bb41e20a1f23d9ed9f2c1291ce
Francis LE ROY <francis.leroy.ch@gmail.com>
2018-03-21 16:18:07 +0100

    cd fixed

--- src/builtins/help.c
+++ src/builtins/help.c
@@ -6,7 +6,7 @@
 /*   By: vbaudot <vbaudot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/28 12:41:02 by vbaudot           #+#    #+#             */
-/*   Updated: 2018/02/28 15:26:33 by vbaudot          ###   ########.fr       */
+/*   Updated: 2018/03/21 16:18:16 by fle-roy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ int	mini_help(char **args)
 		ft_putendl("help: Too many arguments");
 		return (1);
 	}
-	ft_printf("LEROY Francis and FAUTIER Romain\'s Minishell\n");
+	ft_printf("LEROY Francis and FAUTIER Romain\'s 21sh\n");
 	ft_printf("Type program names and arguments,");
-	ft_printf("and hit enter.\nThe following are built in:\n\n");
+	ft_printf("and hit enter.\nThe following function are builtins:\n\n");
 	if (args[0] == NULL)
 		return (1);
-	ft_putendl("->>  cd\n->>  setenv\n->>  unsetenv\n->>  env");
+	ft_putendl("->>  cd \n->>  setenv\n->>  unsetenv\n->>  env");
 	ft_putendl("->>  echo\n->>  exit\n->>  help\n");
-	ft_printf("Use the man command for information on other programs.\n");
+	ft_printf("Use the man command for more informations on other programs.\n");
 	return (1);
 }
