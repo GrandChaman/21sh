6f1a4d6ae81e98a38a40f0e8c087280f5d3d22c2
rfautier <rfautier@student.42.fr>
2018-03-22 11:17:39 +0100

    error msg

--- src/exec/check_pipe.c
+++ src/exec/check_pipe.c
@@ -17,26 +17,26 @@ void	check_pipe(t_parser *parser, int x, t_dup *r_dup)
 	if (!(parser[x].input.pipe) && parser[x].output.pipe)
 	{
 		if (pipe(r_dup->p) == -1)
-			exit(0);
+			ft_perror("pipe", "Pipe failed. Aborting");
 		if (dup2(r_dup->p[1], 1) == -1)
-			exit(0);
+			ft_perror("dup", "Dup failed. Aborting");
 	}
 	else if (!(parser[x].output.pipe) && parser[x].input.pipe)
 	{
 		close(r_dup->p[1]);
 		if (dup2(r_dup->stdin_copy, 1) == -1)
-			exit(0);
+			ft_perror("dup", "Dup failed. Aborting");
 		if (dup2(r_dup->p[0], 0) == -1)
-			exit(0);
+			ft_perror("dup", "Dup failed. Aborting");
 	}
 	else if (parser[x].input.pipe && parser[x].output.pipe)
 	{
 		close(r_dup->p[1]);
 		if (dup2(r_dup->p[0], 0) == -1)
-			exit(0);
+			ft_perror("dup", "Dup failed. Aborting");
 		if (pipe(r_dup->p) == -1)
-			exit(0);
+			ft_perror("Pipe", "Pipe failed. Aborting");
 		if (dup2(r_dup->p[1], 1) == -1)
-			exit(0);
+			ft_perror("dup", "Dup failed. Aborting");
 	}
 }
