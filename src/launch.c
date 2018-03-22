54b6ba0a9fb3be62ed57942805b2d4c9a37f65a1
Francis LE ROY <francis.leroy.ch@gmail.com>
2018-03-22 20:59:15 +0100

    Wait for all process

--- src/launch.c
+++ src/launch.c
@@ -6,7 +6,7 @@
 /*   By: vbaudot <vbaudot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/28 12:40:22 by vbaudot           #+#    #+#             */
-/*   Updated: 2018/03/22 13:14:28 by fle-roy          ###   ########.fr       */
+/*   Updated: 2018/03/22 17:11:35 by fle-roy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,32 +23,32 @@ static int	check_if_can_exec(t_bin_hash *bin, char *arg)
 	return (0);
 }
 
-int			launch(char **args, t_list **head, t_bin_hash_table *ht,
+t_wait_el	launch(char **args, t_list **head, t_bin_hash_table *ht,
 	t_parser parser)
 {
-	pid_t		pid;
+	t_wait_el	el;
 	int			status;
 	t_bin_hash	*bin;
 	char		**env;
 
 	status = 0;
 	bin = NULL;
+	el.pid = -1;
 	if (!ft_strchr(args[0], '/'))
 		bin = get_elem_from_ht(ht, args[0]);
 	if (check_if_can_exec(bin, args[0]))
-		return (0);
+		return (el);
 	env = list_tochar2d(*head);
-	pid = fork();
-	if (pid == 0)
+	el.pid = fork();
+	if (el.pid == 0)
 	{
 		execve((bin ? bin->path : args[0]), &args[0], env);
 		ft_printf("21sh: %s: execve failed.\n", args[0]);
 		exit(-1);
 	}
-	else if (pid < 0)
+	else if (el.pid < 0)
 		ft_putendl("21sh: fork error\n");
-	else if (!parser.output.pipe)
-		waitpid(pid, &status, WUNTRACED);
+	el.is_piped = parser.output.pipe;
 	ft_free2d((void **)env);
-	return (status);
+	return (el);
 }
