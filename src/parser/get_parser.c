8264f64314e9881914ba6e4821c72297ec5f0e1e
rfautier <rfautier@student.42.fr>
2018-04-04 15:58:42 +0200

    create file before execute

--- src/parser/get_parser.c
+++ src/parser/get_parser.c
@@ -12,6 +12,47 @@
 
 #include "ft_sh.h"
 
+static void	creat_file_input(t_parser parser)
+{
+	int fd;
+	int i;
+
+	i = 0;
+	while (parser.input.meta)
+	{
+		if (parser.input.meta[i].name[0] != '&')
+		{
+			if ((fd = open(parser.input.meta[i].name, O_RDWR | O_CREAT
+				| O_EXCL, 0777)) != -1)
+				close(fd);
+		}
+		if (parser.input.meta[i].next_exist == 0)
+			break ;
+		i++;
+	}
+}
+
+void		create_file(t_parser parser)
+{
+	int fd;
+	int i;
+
+	i = 0;
+	while (parser.output.meta)
+	{
+		if (parser.output.meta[i].name[0] != '&')
+		{
+			if ((fd = open(parser.output.meta[i].name, O_RDWR |
+				O_CREAT | O_EXCL, 0777)) != -1)
+				close(fd);
+		}
+		if (parser.output.meta[i].next_exist == 0)
+			break ;
+		i++;
+	}
+	creat_file_input(parser);
+}
+
 t_parser	*get_parser(char *ori)
 {
 	int			stock;
